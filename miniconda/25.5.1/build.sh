d413e6213f4a3d5da1d758adba0de02529c5d68a
Christopher Hosken <hoskenchristopher@gmail.com>
2025-08-14 20:43:06 +0100

    removed miniconda

--- miniconda/25.5.1/build.sh
+++
@@ -1,9 +0,0 @@
-VERSION=$(basename "$(dirname "$0")")
-
-PYTHON_VERSION="313"
-
-INSTALLER="Miniconda3-py"$PYTHON_VERSION"_"$VERSION"-1-Linux-x86_64.sh"
-
-wget https://repo.anaconda.com/miniconda/$INSTALLER
-chmod +x "./$INSTALLER"
-"./$INSTALLER" -b -f -p $REZ_BUILD_INSTALL_PATH
