33efd93b47375df23c1cc15d9439ed76184ae35a
Christopher Hosken <hoskenchristopher@gmail.com>
2025-08-14 20:42:51 +0100

    removed code

--- vscode/1.102.2/build.sh
+++
@@ -1,7 +0,0 @@
-VERSION=$(basename "$(dirname "$0")")
-
-wget https://update.code.visualstudio.com/$VERSION/linux-x64/stable -O vscode.tgz
-
-if [ -f vscode.tgz ]; then
-    tar -xzf vscode.tgz -C $REZ_BUILD_INSTALL_PATH --strip-components=1
-fi
